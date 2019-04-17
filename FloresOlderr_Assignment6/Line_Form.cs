6933339c7d1cd5b98364ceca71c3667d3336da1e
JoshuaAaronFlores <JoshuaAaronFlores@users.noreply.github.com>
2019-04-16 20:31:20 -0500

    Line chart pulling data from List.

--- FloresOlderr_Assignment6/Line_Form.cs
+++ FloresOlderr_Assignment6/Line_Form.cs
@@ -15,6 +15,7 @@ namespace FloresOlderr_Assignment6
     {
         LineData LD;
         public List<string> data_values;
+        enum Month { January, February, March, April, May, June, July, August, September, October, November, December };
 
         public Line_Form(LineData LD)
         {
@@ -40,32 +41,62 @@ namespace FloresOlderr_Assignment6
 
         private void Line_Form_Load(object sender, EventArgs e)
         {
+            List<string> data_values_2014 = new List<string>();
+            List<string> data_values_2015 = new List<string>();
+            List<string> data_values_2016 = new List<string>();
+            List<string> data_values_2017 = new List<string>();
+
+            Title Line_Chart_Title = new Title("Bike Share Statistics" + Environment.NewLine + "(Amount rented per month)", Docking.Top, new Font("Yu Gothic", 8, FontStyle.Bold), Color.Black);
+
+            chart1.Titles.Add(Line_Chart_Title);
+
+            for(int i = 0; i < 48; i++)
+            {
+                if( i < 12 )
+                {
+                    data_values_2014.Add(data_values.ElementAt(i));
+                }
+                else if ( i >= 12 && i < 24)
+                {
+                    data_values_2015.Add(data_values.ElementAt(i));
+                }
+                else if (i >= 24 && i < 36)
+                {
+                    data_values_2016.Add(data_values.ElementAt(i));
+                }
+                else
+                {
+                    data_values_2017.Add(data_values.ElementAt(i));
+                }
+            }
+           
+
             var series14 = new Series("Bike Share 2014");
             var series15 = new Series("Bike Share 2015");
+            var series16 = new Series("Bike Share 2016");
+            var series17 = new Series("Bike Share 2017");
+
             string[] months = { "Jan", "Feb", "March", "April", "May", "June",
                                  "July", "Sep", "Oct", "Nov", "Dec"};
+
+            int[] month_numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
             series14.ChartType = SeriesChartType.Line;
             series15.ChartType = SeriesChartType.Line;
+            series16.ChartType = SeriesChartType.Line;
+            series17.ChartType = SeriesChartType.Line;
+
             // Frist parameter is X-Axis and Second is Collection of Y- Axis
 
-            //foreach(string data in data_values)
-            //{
-            //    series.Points.DataBindXY(new<List>  data );
-            //}
-            //series.Points.DataBindXY(new[] { 2001, 2002, 2003, 2004 }, new[] { 100, 200, 90, 150 });
-            series14.Points.DataBindXY(
-                new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, 
-                new[] { 3224, 3546.2, 2872.5, 3188, 3212.8, 3205.7, 3124.2, 3160.7, 3400.1, 3490, 3373, 3328 }
-                );
-
-            series15.Points.DataBindXY(
-                new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-                new[] { 2891.6, 2798.8, 3266, 3681, 3582, 3328.2, 3487, 3579, 3791, 4234, 3910, 4175 });
-
-
+            series14.Points.DataBindXY( month_numbers, data_values_2014 );
+            series15.Points.DataBindXY( month_numbers, data_values_2015 );
+            series16.Points.DataBindXY( month_numbers, data_values_2016 );
+            series17.Points.DataBindXY( month_numbers, data_values_2017 );
 
             chart1.Series.Add(series14);
             chart1.Series.Add(series15);
+            chart1.Series.Add(series16);
+            chart1.Series.Add(series17);
         }
     }
 }
